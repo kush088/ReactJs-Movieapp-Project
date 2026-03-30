dd4afd124c2c3e49ab8fc8c737fe0b1f3ca47f22
kush088 <kushparekh01@email.com>
2026-03-30 09:28:45 +0530

    connect cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "abc123xyz",   // 👈 paste your projectId here
+  projectId: "eia7yr",   // 👈 paste your projectId here
   e2e: {
     baseUrl: "http://localhost:5173",
   },
